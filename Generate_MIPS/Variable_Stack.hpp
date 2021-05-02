8ad95eeb918a46a1faa1f1f6025055970075f679
XinfeiLI <118010151@link.cuhk.edu.cn>
2021-05-03 02:33:58 +0800

    Early work for Variable_Stack class

--- Generate_MIPS/Variable_Stack.hpp
+++ Generate_MIPS/Variable_Stack.hpp
@@ -5,21 +5,37 @@
 #include <vector>
 #include <stack>
 #include <algorithm>
+#include <unordered_map>
+#include <string>
+
+struct storage_set{
+    int memory_index;
+    int value;
+    storage_set(int a,int b) : memory_index(a),value(b){}
+}
+
+enum operations{lsquare,_not,_and,_or,add,minus,multiple,divide,lt,gt,
+                equal,noteq,lteq,gteq,shlop,shrop,andand,oror,paren,minus}
 
 class Variable_Stack{
     private:
-        std::vector<int> table;
+        std::unordered_map<std::string,storage_set> variable_map;
+        std::unordered_map<int,storage_set> temp_var_map;
+        int index = 0, count = 0;
     public:
-        Variable_Stack();
-        ~Variable_Stack();
-};
+        int expression_process(int value);
+        int operations_expression(operations operation){}
 
-Variable_Stack::Variable_Stack(){
-    
+int Variable_Stack::expression_process(int value){
+    temp_var_map[count] = storage_set(index,value);
+    count++;
+    index++;
+    return count;
 }
 
-Variable_Stack::~Variable_Stack(){
-
+int Variable_Stack::operations_expression(operations operation){
+    switch (operation){
+        case 1:
 }
 
 #endif
