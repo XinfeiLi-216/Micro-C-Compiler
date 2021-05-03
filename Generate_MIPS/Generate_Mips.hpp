8913836c436ddbbd4a555708aa52f5526c2db6ad
lixinfei <xinfeili@link.cuhk.edu.cn>
2021-05-04 03:23:03 +0800

    Bugs of to_string solved

--- Generate_MIPS/Generate_Mips.hpp
+++ Generate_MIPS/Generate_Mips.hpp
@@ -27,64 +27,64 @@ class Generate_Mips{
                 case 3:
                     break;
                 case 4:
-                    text_segment.push_back("lw $s1,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("lw $s2,"+to_string(idx2*4)+"($t0)");
+                    text_segment.push_back("lw $s1,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("lw $s2,"+std::to_string(idx2*4)+"($t0)");
                     text_segment.push_back("add $s0,$s1,$s2");
-                    text_segment.push_back("sw $s0,"+to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("sw $s0,"+std::to_string(idx1*4)+"($t0)");
                     break;
                 case 5:
-                    text_segment.push_back("lw $s1,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("lw $s2,"+to_string(idx2*4)+"($t0)");
+                    text_segment.push_back("lw $s1,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("lw $s2,"+std::to_string(idx2*4)+"($t0)");
                     text_segment.push_back("sub $s0,$s1,$s2");
-                    text_segment.push_back("sw $s0,"+to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("sw $s0,"+std::to_string(idx1*4)+"($t0)");
                     break;
                 case 6:
-                    text_segment.push_back("lw $s1,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("lw $s2,"+to_string(idx2*4)+"($t0)");
+                    text_segment.push_back("lw $s1,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("lw $s2,"+std::to_string(idx2*4)+"($t0)");
                     text_segment.push_back("mul $s0,$s1,$s2");
-                    text_segment.push_back("sw $s0,"+to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("sw $s0,"+std::to_string(idx1*4)+"($t0)");
                     break;
                 case 7:
-                    text_segment.push_back("lw $s1,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("lw $s2,"+to_string(idx2*4)+"($t0)");
+                    text_segment.push_back("lw $s1,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("lw $s2,"+std::to_string(idx2*4)+"($t0)");
                     text_segment.push_back("div $s1,$s2");
-                    text_segment.push_back("sw $lo,"+to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("sw $lo,"+std::to_string(idx1*4)+"($t0)");
                     break;
                 case 8:
-                    text_segment.push_back("lw $s1,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("lw $s2,"+to_string(idx2*4)+"($t0)");
+                    text_segment.push_back("lw $s1,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("lw $s2,"+std::to_string(idx2*4)+"($t0)");
                     text_segment.push_back("slt $s0,$s1,$s2");
-                    text_segment.push_back("sw $s0,"+to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("sw $s0,"+std::to_string(idx1*4)+"($t0)");
                     break;
                 case 9:
                     /* use beq slt to design the greater than */
-                    text_segment.push_back("lw $s1,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("lw $s2,"+to_string(idx2*4)+"($t0)");
+                    text_segment.push_back("lw $s1,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("lw $s2,"+std::to_string(idx2*4)+"($t0)");
                     text_segment.push_back("slt $t3,$t1,$t2");
                     text_segment.push_back("li $t4,1");
-                    text_segment.push_back("sw $zero,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("beq $t3,$t4,gt_not_label"+to_string(gt_not_count));
-                    text_segment.push_back("beq $t1,$t2,gt_not_label"+to_string(gt_not_count));    
-                    text_segment.push_back("sw $t4,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("gt_not_label"+to_string(gt_not_count++)+":");                 
+                    text_segment.push_back("sw $zero,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("beq $t3,$t4,gt_not_label"+std::to_string(gt_not_count));
+                    text_segment.push_back("beq $t1,$t2,gt_not_label"+std::to_string(gt_not_count));    
+                    text_segment.push_back("sw $t4,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("gt_not_label"+std::to_string(gt_not_count++)+":");                 
                     break;
                 case 10:
-                    text_segment.push_back("lw $s1,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("lw $s2,"+to_string(idx2*4)+"($t0)");
+                    text_segment.push_back("lw $s1,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("lw $s2,"+std::to_string(idx2*4)+"($t0)");
                     text_segment.push_back("li $t3,1");
-                    text_segment.push_back("sw $t3,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("beq $s1,$s2,equal_label"+to_string(equal_count));
-                    text_segment.push_back("sw $zero,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("equal_label"+to_string(equal_count++)+":");
+                    text_segment.push_back("sw $t3,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("beq $s1,$s2,equal_label"+std::to_string(equal_count));
+                    text_segment.push_back("sw $zero,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("equal_label"+std::to_string(equal_count++)+":");
                     break;
                 case 11:
-                    text_segment.push_back("lw $s1,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("lw $s2,"+to_string(idx2*4)+"($t0)");
+                    text_segment.push_back("lw $s1,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("lw $s2,"+std::to_string(idx2*4)+"($t0)");
                     text_segment.push_back("li $t3,1");
-                    text_segment.push_back("sw $zero,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("beq $s1,$s2,equal_label"+to_string(equal_count));
-                    text_segment.push_back("sw $t3,"+to_string(idx1*4)+"($t0)");
-                    text_segment.push_back("equal_label"+to_string(equal_count++)+":");
+                    text_segment.push_back("sw $zero,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("beq $s1,$s2,equal_label"+std::to_string(equal_count));
+                    text_segment.push_back("sw $t3,"+std::to_string(idx1*4)+"($t0)");
+                    text_segment.push_back("equal_label"+std::to_string(equal_count++)+":");
                     break;
                 case 12:
                     std::cout<<"Case 12 Execution"<<std::endl;
